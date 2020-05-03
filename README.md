e518a96b78c8f94942cb6f5cfebbc6a6bf20ed93
Wayne <wayne_boyles@hotmail.com>
2020-05-03 13:55:33 -0500

    Initial Commit
    First commit of Plaster template.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# psmodule-template
-Template Repository for PowerShell Modules
+# <%= $PLASTER_PARAM_ModuleName %>
+
+<%= $PLASTER_PARAM_ModuleDesc %>
+
+## Getting Started
+
+Install from the PSGallery and Import the module
+
+    Install-Module <%= $PLASTER_PARAM_ModuleName %>
+    Import-Module <%= $PLASTER_PARAM_ModuleName %>
---
+++ Source/Module.psm1
@@ -0,0 +1,19 @@
+[CmdletBinding()]
+Param()
+
+Write-Verbose "Importing Functions..."
+
+foreach ($folder in @('Enums', 'Classes', 'Private', 'Public'))
+{
+    $root = Join-Path -Path $PSScriptRoot -ChildPath $folder
+
+    if (Test-Path -Path $root)
+    {
+        Write-Verbose " - Importing $($Root)"
+        
+        $files = Get-ChildItem -Path $root -Filter *.ps1 -Recurse
+        $files | Where-Object { $_.Name -notlike '*.Tests.ps1' } | ForEach-Object { Write-Verbose $_.BaseName }
+    }
+}
+
+Export-ModuleMember -Function (Get-ChildItem -Path "$PSScriptRoot\Public\*.ps1").BaseName
